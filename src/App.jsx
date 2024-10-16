499646e669b54dc962083e47ca584234ba5439a5
jatinmalik-11 <epoj1108@gmail.com>
2024-10-16 10:38:25 +0530

    All files added successully

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+import Main from './components/Main/Main'
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
